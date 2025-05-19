72aaee1c9753b4d041636960bc9f66b7c6c8fb9c
ZakIadhik <nazarov.ramin.2004@mail.ru>
2025-05-19 20:58:03 +0400

    Add files via upload

---
+++ Practice operators/Task-3.js
@@ -0,0 +1,51 @@
+console.log("сравнение чисел")
+console.log(" ")
+const a = 17
+const b = 5
+
+console.log("a > 5", a > b)
+console.log("a <= 5", a <= b)
+console.log("a == 5", a == b)
+console.log("a != 5", a != b)
+
+console.log(" ")
+console.log("сравнение строк")
+console.log(" ")
+
+console.log("'a' < 'b'", 'a' < 'b')
+console.log("'ab' > 'a'", 'ab' > 'a')
+
+console.log(" ")
+console.log("строгое сравнение")
+console.log(" ")
+
+console.log("17 === 1", 17 === 1)
+console.log("17 === 17", 17 === 17)
+console.log("17 === '17'", 17 === '17')
+console.log("17 === true", 17 === true)
+console.log("'0' === ''", '0' === '')
+console.log("true === false", true === false)
+console.log("true === true", true === true)
+console.log("null === undefined", null === undefined)
+console.log("false === 0", false === 0)
+
+console.log(" ")
+console.log("нестрогое сравнение")
+console.log(" ")
+
+console.log("17 == '17'", 17 == '17')
+console.log("'0' == ''", '0' == '')
+console.log("0 == ''", 0 == '')
+console.log("null == undefined", null == undefined)
+console.log("false == 0", false == 0)
+
+console.log(" ")
+console.log("осторожно c null и undefined")
+console.log(" ")
+
+console.log("undefined == null", undefined == null)
+console.log("undefined == 0", undefined == 0)
+console.log("null == 0", null == 0)
+console.log("undefined < 0", undefined < 0)
+console.log("undefined > 0", undefined > 0)
+
